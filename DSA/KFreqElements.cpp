98340a80b992ba12c1082c40fcfe71c2286e5b5e
codevory <shahijahan.codes@gmail.com>
2026-01-27 13:20:20 +0530

    added problem solving approach guide

--- DSA/KFreqElements.cpp
+++ DSA/KFreqElements.cpp
@@ -10,6 +10,13 @@
 // Input: nums = [1,2,1,2,1,2,3,1,3,2], k = 2
 // Output: [1,2]
 
+// Approach
+//  Step 1 create a unordere_map suppose count & add values one by one in it via loop to count exactly how many times do each number appeared.;
+// step 2 Convert map to vector of pairs, create a array of vector with pair in it both of int suppose you named it arr & push all values of count in it in reverse order ie. second followed by first.
+//  step 3 Sort arr in reverse order by arr.rbegin(),arr.rend() , by this way highest values will start from first . eg.  [(4,1), (4,2), (2,3)] here 4 is freq , 1 is numb.
+// step 4 create new array of vector eg ans, & push values of arr.second(values not freq '.' we need to return values not freq) untill loop ends ie n = k;
+// now return .
+
 #include <iostream>
 #include <vector>
 #include <unordered_map>
