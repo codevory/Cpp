7f98b94988b2d1d71aee50b2977b627aeadb2d65
codevory <shahijahan.codes@gmail.com>
2026-03-02 15:48:23 +0530

    solved second_Small & second_highest problem with optimum complexity

--- DSA/smallestLargest.cpp
+++ DSA/smallestLargest.cpp
@@ -11,9 +11,7 @@ class Solution
 public:
     vector<int> smallestAndLargest(vector<int> &arr)
     {
-        int highest = arr[0];
         int secHigh = arr[0];
-        int low = arr[0];
         int secLow = arr[0];
         vector<int> ans(2, 0);
         ans.push_back(secHigh);
@@ -46,6 +44,48 @@ public:
         cout << "SecHigh : " << secHigh << endl;
         return ans;
     }
+    vector<int> optimSmallestAndLargest(vector<int> &arr)
+    {
+        int highest = INT_MIN;
+        int second_Max = INT_MIN;
+        int lowest = INT_MAX;
+        int second_Min = INT_MAX;
+        int n = arr.size();
+        vector<int> ans;
+
+        // find lowest & highest
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] < lowest)
+            {
+                lowest = arr[i];
+            }
+            if (arr[i] > highest)
+            {
+                highest = arr[i];
+            }
+        }
+
+        // find secondLowest & secondHighest
+        for (int j = 0; j < n; j++)
+        {
+            // second_lowest
+            if (arr[j] < second_Min && arr[j] != lowest)
+            {
+                second_Min = arr[j];
+            }
+
+            // second_highest
+            if (arr[j] > second_Max && arr[j] != highest)
+            {
+                second_Max = max(arr[j], second_Max);
+            }
+        }
+        cout << highest << " " << lowest << " " << endl;
+        ans.push_back(second_Min);
+        ans.push_back(second_Max);
+        return ans;
+    }
 };
 
 int main()
@@ -53,9 +93,10 @@ int main()
     vector<int> arr = {1, 2, 4, 7, 7, 5};
     vector<int> arr2 = {3, 3, 4, 8, 7, 9, 11, 13, 13};
     Solution obj;
-    vector<int> out = obj.smallestAndLargest(arr2);
-    /* for (int x : out)
-     {
+    // vector<int> out = obj.smallestAndLargest(arr2);
+    vector<int> out = obj.optimSmallestAndLargest(arr);
+    for (int x : out)
+    {
         cout << x << " ";
-     } */
+    }
 }
