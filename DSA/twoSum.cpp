0dfdfcca5570aa87a9efb0158d4186b0312d5d7d
codevory <shahijahan.codes@gmail.com>
2026-03-14 11:13:07 +0530

    two sum optimalspproach

--- DSA/twoSum.cpp
+++ DSA/twoSum.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution
@@ -27,7 +28,7 @@ public:
         }
         return indices;
     }
-    vector<int> optimalTwoSum(vector<int> &arr, int tar)
+    vector<int> betterTwoSum(vector<int> &arr, int tar)
     {
         int n = arr.size();
         if (n == 1)
@@ -45,6 +46,35 @@ public:
                 return {mp[rem], i};
             }
             mp[arr[i]] = i;
+        }
+        return {-1, -1};
+    }
+    vector<int> optimalTwoSum(vector<int> &arr, int tar)
+    {
+        int n = arr.size();
+        vector<pair<int, int>> numsWithIndex;
+        for (int i = 0; i < n; i++)
+        {
+            numsWithIndex.push_back({arr[i], i});
+        }
+
+        int lp = 0;
+        int rp = n - 1;
+        sort(numsWithIndex.begin(), numsWithIndex.end());
+
+        while (lp <= rp)
+        {
+            int sum = numsWithIndex[lp].first + numsWithIndex[rp].first;
+            if (sum == tar)
+            {
+                return {numsWithIndex[lp].second, numsWithIndex[rp].second};
+            }
+            else if (sum < tar)
+                lp++;
+            else
+            {
+                rp--;
+            }
         }
         return {-1, -1};
     }
