762f27540b92ed00319bf15f06042a2d2e279839
codevory <shahijahan.codes@gmail.com>
2026-04-02 13:19:19 +0530

    better approach solution

--- DSA/set-matrix-zero.cpp
+++ DSA/set-matrix-zero.cpp
@@ -33,11 +33,41 @@ public:
             }
         }
 
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < m; i++)
         {
-            for (int j = 0; j < m; j++)
+            for (int j = 0; j < n; j++)
             {
                 if (matrix[i][j] == -1)
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+    void setZerosBetter(vector<vector<int>> &matrix)
+    {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<int> row(m, 0);
+        vector<int> col(n, 0);
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (row[i] == 1 || col[j] == 1)
                 {
                     matrix[i][j] = 0;
                 }
@@ -52,7 +82,7 @@ int main()
     vector<vector<int>> matrix2 = {{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
     vector<vector<int>> matrix3 = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
     Solution sol;
-    sol.setZeroes(matrix1);
+    sol.setZerosBetter(matrix1);
     for (auto val : matrix1)
     {
         for (auto v : val)
